f46b6b70b549d0fe9103abbb9b52ab0b655dd6ab
Samuel <samuel@askia.com>
2020-03-10 18:03:09 +0100

    Use transaction

--- datastore/mysql.go
+++ datastore/mysql.go
@@ -53,15 +53,26 @@ func (datastore *MySQLDatastore) FindByMostUsedQueryString() (models.Request, er
 
 // Store takes a Request as parameter and store it to the database
 func (datastore *MySQLDatastore) Store(request *models.Request) error {
-	req, err := datastore.db.Prepare(`INSERT INTO requests (int1_param, int2_param, limit_param, str1_param, str2_param)
-										VALUES (?, ?, ?, ?, ?)`)
+	tx, err := datastore.db.Begin()
 	if err != nil {
 		return err
 	}
-	req.Exec(request.Int1Param,
+
+	_, err = tx.Exec(`INSERT INTO requests (int1_param, int2_param, limit_param, str1_param, str2_param)
+							VALUES (?, ?, ?, ?, ?)`,
+		request.Int1Param,
 		request.Int2Param,
 		request.LimitParam,
 		request.Str1Param,
 		request.Str2Param)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
 	return nil
 }
