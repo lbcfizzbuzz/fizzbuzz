70991f31d5bce4c2b027ef3208dd68efa1a7a37b
Samuel TOUSSAY <samuel@askia.com>
2020-03-10 03:19:04 +0100

    Add service tests

---
+++ tests/service_test.go
@@ -0,0 +1,63 @@
+package tests
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	conf "github.com/samyy321/fizzbuzz/config"
+	ds "github.com/samyy321/fizzbuzz/datastore"
+	"github.com/samyy321/fizzbuzz/models"
+	"github.com/samyy321/fizzbuzz/service"
+	"testing"
+)
+
+func TestService(t *testing.T) {
+	// Read the configuration file
+	config := conf.Configuration{}
+	if err := config.Read("../config/config-test.json"); err != nil {
+		t.Errorf(err.Error())
+	}
+
+	// Get the datastore
+	datastore, err := ds.GetDatastore(config)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	err = datastore.Init()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	// Create a first request
+	request := models.Request{
+		Int1Param:  3,
+		Int2Param:  5,
+		LimitParam: 10,
+		Str1Param:  "fizz",
+		Str2Param:  "buzz"}
+
+	for i := 0; i < 3; i++ {
+		_, err := service.GetFizzbuzzStrings(datastore, &request)
+		if err != nil {
+			t.Errorf("An unhandled error occured")
+		}
+	}
+
+	// Change the request and use more than the first one
+	request.Int2Param = 6
+	for i := 0; i < 4; i++ {
+		_, err := service.GetFizzbuzzStrings(datastore, &request)
+		if err != nil {
+			t.Errorf("An unhandled error occured in GetFizzbuzzStrings()")
+		}
+	}
+
+	mostUsedRequest, err := service.GetMostUsedQueryString(datastore)
+	if err != nil {
+		t.Errorf("An unhandled error occured in GetMostUsedQueryString()")
+	}
+
+	// We need to set the count to be able to compare correctly
+	request.Count = mostUsedRequest.Count
+	if mostUsedRequest != request {
+		t.Errorf("The most used request is not the expected one")
+	}
+}
